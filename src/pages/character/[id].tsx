61ffd963fcc070b74927a230f94af5a5242ca1b8
Simon Lanza Fiorito <simonlanza99@gmail.com>
2023-08-18 12:18:48 -0300

    deletion of copys

--- src/pages/character/[id].tsx
+++ src/pages/character/[id].tsx
@@ -5,6 +5,8 @@ import {useState, useEffect} from "react";
 import {Character} from "../../interface/character";
 import {get} from "http";
 import {Card} from "@/components/ui/card";
+import { GetStaticPaths } from "next";
+import { getCharacter } from "@/service";
 
 const CharacterPage = () => {
 	const router = useRouter();
@@ -29,5 +31,26 @@ const CharacterPage = () => {
 		</Layout>
 	);
 };
+
+export const getStaticPath: GetStaticPaths = async ({ locales }) => {
+	const idiomas = locales as string [];
+	const data = await getCharacters()
+	const paths = data.flatMap((character) => (
+		idiomas.map((idioma) => ({params : {id: character.tail}, locale: idioma}))
+	))
+
+	return {
+		paths: [],
+		fallback: true,
+	}
+}
+
+export const getStatiProps: GetStaticProps = async ({ params }) => {
+
+	const id = params?.id as string;
+
+	const character = await getCharacter();
+
+}
 
 export default CharacterPage;
